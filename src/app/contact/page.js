8a19837dd8b5352e0233c9525cd1ea8f4d17adcd
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-10 16:00:00 +0600

    map add

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,11 +4,30 @@ import styles from "./contact.module.css";
 import ContactForm from "@/app/components/ContactForm";
 
 const Contact = () => {
+    <style>
+        {`
+      .mapouter {
+        position: relative;
+        text-align: right;
+        width: 100%;
+        height: 400px;
+      }
+      .gmap_canvas {
+        overflow: hidden;
+        background: none!important;
+        width: 100%;
+        height: 400px;
+      }
+      .gmap_iframe {
+        height: 400px!important;
+      }
+    `}
+    </style>
     return (
         <>
             <div className={styles.container}>
-            <h1>Contact Us</h1>
-            <ContactCard />
+                <h1>Contact Us</h1>
+                <ContactCard />
 
                 <section className={styles.contact_section}>
                     <h2>We'd love to hear <span> from you </span></h2>
@@ -18,9 +37,16 @@ const Contact = () => {
             </div>
 
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.2547158278226!2d73.91419611127971!3d18.562551782466336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20-%20Viman%20Nagar!5e0!3m2!1sen!2sin!4v1681696533582!5m2!1sen!2sin"
-                width={100} height={450} style={{border: 0}} allowFullScreen="" loading="lazy" className={styles.mapping}
+                src="https://maps.google.com/maps?width=650&amp;height=400&amp;hl=en&amp;q=savar,Dhaka , Bangladesh&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                width={100} height={450} style={{ border: 0 }} allowFullScreen="" loading="lazy" className={styles.mapping}
                 referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+            {/* <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=650&amp;height=400&amp;hl=en&amp;q=savar,Dhaka , Bangladesh&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://thepasswordgame.com/">The Password Game</a>
+                </div>
+            </div> */}
+
 
         </>
     );
